c6052f575544eddca3a0a8544d05ad9c50981e63
Alessandro <dorigo.alessandro@gmail.com>
2020-02-25 16:05:33 +0100

    ft_strlen done strlcpy on route

---
+++ main.c
@@ -0,0 +1,21 @@
+// // #include <string.h>
+
+// int main(void)
+// {
+// 	 char c[32] = "bon";
+// 	 char *d = "b";
+    
+// 	printf("%s\n", c);
+// 	printf("%s\n", strcpy(c, d));
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    char ch_arr1[20] = "heyya";
+
+    strcpy(ch_arr1, "Greece"); // copy Greece to ch_arr1
+
+    return 0;
+}
